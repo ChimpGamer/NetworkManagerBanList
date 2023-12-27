093b78d93caf5559496a0a11741a7b5c1a8cd3df
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-12-27 20:46:36 +0100

    Added german translation in my best german possible

---
+++ lang/de/messages.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @author ChimpGamer
+ * @date 27-12-2023
+ */
+
+return [
+    'navbar_home' => '<i class="fa fa-house"></i> Startseite',
+    'navbar_bans' => '<i class="fa fa-gavel"></i> Verbannungen <span class="badge badge-danger">:total_bans</span>',
+    'navbar_mutes' => '<i class="fa fa-comment-slash"></i> Gestummte <span class="badge badge-danger">:total_mutes</span>',
+    'navbar_kicks' => '<i class="fa fa-eject"></i> Gekickte <span class="badge badge-danger">:total_kicks</span>',
+    'navbar_warns' => '<i class="fa fa-triangle-exclamation"></i> Verwarnungen <span class="badge badge-danger">:total_warns</span>',
+
+    'title_bans' => 'Verbannungen',
+    'title_mutes' => 'Gestummte',
+    'title_kicks' => 'Gekickte',
+    'title_warns' => 'Verwarnungen',
+
+    'variable_playername' => 'Spielername',
+    'variable_punisher' => 'Bestrafer',
+    'variable_reason' => 'Grund',
+
+    'variable_ban' => 'Verbannungen',
+    'variable_banned_on' => 'Gebannt Am',
+    'variable_banned_until' => 'Gebannt Bis',
+
+    'variable_mute' => 'Stumm',
+    'variable_muted_on' => 'Stummgeschaltet Am',
+    'variable_muted_until' => 'Stummgeschaltet Bis',
+
+    'variable_kick' => 'Kick',
+    'variable_kicked_on' => 'Gekickt Am',
+
+    'variable_warn' => 'Verwarnung',
+    'variable_warned_on' => 'Verwarnt Am',
+
+    'variable_permanent' => 'Permanent',
+    'variable_ip_ban' => 'IP-Verbannungen',
+
+    'variable_ip_mute' => 'IP-Stumm',
+
+    'variable_expires' => 'Läuft ab in',
+    'variable_server' => 'Server',
+    'variable_status' => 'Status',
+    'variable_global' => 'Global',
+    'variable_active' => 'AKTIV',
+    'variable_expired' => 'ABGELAUFEN',
+
+    'variable_datetime_on' => 'am',
+
+    'player_joined' => '<i class="fa fa-calendar-days"></i> Beigetreten',
+    'player_last_login' => '<i class="fa fa-clock-rotate-left"></i> Letzte Anmeldung',
+    'player_last_logout' => '<i class="fa fa-delete-left"></i> Letzte Abmeldung',
+    'player_total_playtime' => '<i class="fa fa-clock"></i> Gesamtspielzeit',
+    'player_current_minecraft_version' => '<i class="fa fa-screwdriver-wrench"></i> Aktuelle Minecraft-Version',
+
+    'player_information' => 'Information',
+    'player_punishments' => 'Strafen :punishments',
+    'player_bans' => 'Verbannungen :bans',
+    'player_mutes' => 'Gestummte :mutes',
+    'player_kicks' => 'Gekickte :kicks',
+    'player_warns' => 'Verwarnungen :warns',
+
+    'placeholder_search_player' => '&#xf002; Spieler suchen...',
+    'placeholder_search' => '&#xf002; Suchen...',
+
+    'homepage_text' => '<h1>Willkommen auf der NetworkManager Bann Liste.</h1>
+<h5>Diese Seite enthält eine Liste aller Strafen von NetworkManager.</h5>
+            <h5>Gesamtstrafen: <span class="badge badge-danger">:total_punishments</span></h5>',
+
+    '404_title' => '404',
+    '404_text' => '<p class="h2">Leider können wir die von Ihnen gesuchte Seite nicht finden.</p>
+                        <p class="lead">Klicken Sie auf die Schaltfläche unten, um zur Startseite zurückzukehren.</p>',
+    '404_button_text' => 'Klicken Sie hier, um zur Startseite zu gelangen',
+
+    'length_menu' => ':menu Zeilen anzeigen'
+];
