66e3fbf1c5d23e318e94d15f98a4eb91f7b522ec
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-04-13 00:03:52 +0200

    Bump font awesome to 6.5.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
 
     <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet"/>
 
     <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/labels.css') }}" rel="stylesheet"/>
