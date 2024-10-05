53bc60f1bab438ff0a610188ecc3b4757f9675df
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-10-05 16:25:22 +0200

    Console no longer navigates to a non existing player page.

--- resources/views/components/player-link.blade.php
+++ resources/views/components/player-link.blade.php
@@ -1,3 +1,6 @@
 @props(['uuid', 'username'])
-
-<img alt="player head" draggable="false" src="https://minotar.net/avatar/{{$uuid}}/20"> <a wire:navigate href="{{route('player', $uuid)}}">{{ $username }}</a>
+<img alt="player head" draggable="false" src="https://minotar.net/avatar/{{$uuid}}/20"> @if($uuid == 'f78a4d8d-d51b-4b39-98a3-230f2de0c670')
+    <span>{{ $username }}</span>
+@else
+    <a wire:navigate href="{{route('player', $uuid)}}">{{ $username }}</a>
+@endif
